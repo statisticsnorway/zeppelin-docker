c9045ae294d1167348d78b013fd2ca0a0c61a024
Skaar, Bjørn-Andre <bjorn.skaar@ssb.no>
2019-12-06 13:15:12 +0100

    Added error checks to script

--- create-python-env.bash
+++ create-python-env.bash
@@ -4,7 +4,6 @@ python_version_major=2.7
 python_version=${python_version_major}.13
 
 echo -e '\033[0;32m--- Install custom SSB python library (standard python) --- \033[0m'
-pwd
 pip install ssb-pseudonymization
 
 # Install custom SSB python library with conda env
@@ -13,9 +12,15 @@ echo -e '\033[0;32m--- Initialising conda env ---\033[0m'
 conda init bash
 source /root/.bashrc
 conda create -n ssb-env python=$python_version_major pip
+conda env list | grep ssb-env
+if [[ $? != 0 ]]
+then
+    echo -e "\033[0;31m--- Creating ssb-env failed ---\033[0m"
+    exit 1
+fi
+
 echo -e '\033[0;32m--- Activate ssb-env ---\033[0m'
 source activate ssb-env
-conda env list
 pip install ssb-pseudonymization
 echo -e '\033[0;32m--- Packaging ssb-env ---\033[0m'
 conda package --pkg-name ssb-env
@@ -31,7 +36,7 @@ echo -e '\033[0;32m--- Creating zip file ---\033[0m'
 tar -xvf ssb-env-*.tar.bz2 --strip-components=2 lib/python$python_version_major/site-packages/
 if [ ! -r site-packages ]
 then
-    echo "\033[0;31m--- Extracting ssb-env failed (site-packages not found) ---\033[0m"
+    echo -e "\033[0;31m--- Extracting ssb-env failed (site-packages not found) ---\033[0m"
     exit 1
 fi
 
